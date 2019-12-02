63a0c912d7f515db601065a99e6286c769a29f91
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 11:38:16 +0800

    Add all my files

---
+++ Assets/_MyAsset/_Script/VirtualButtonFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonFire : MonoBehaviour, IVirtualButtonEventHandler {
+    private GameObject vbButton1;
+	// Use this for initialization
+	void Start () {
+        vbButton1 = GameObject.Find("FireButton");
+        vbButton1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vr)
+    {
+        CharacterController.isFire = true;
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vr)
+    {
+        CharacterController.isFire = false;
+    }
+}
